bc4792aabe5b5b5e6b695241e8fb40b74ed23c11
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-09-11 02:32:16 +0300

    Added db creation

--- lib/classes/Customer.py
+++ lib/classes/Customer.py
@@ -1,8 +1,23 @@
-import gc
-from classes.Review import Review
+from Review import Review
 
-class Customer:
-    instances=[]
+from sqlalchemy import (create_engine, desc, func,
+    CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
+    Index, Column, DateTime, Integer, String)
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+class Customer(Base):
+    __tablename__='customers'                           #Creates a customers table
+
+    id = Column(Integer(),primary_key=True)
+    name = Column(String())
+    family_name = Column(String())
+
+
 
     def __init__(self,name, family_name):
         self.name =name
@@ -36,4 +51,15 @@ class Customer:
         print(len(self.review_count))
 
     def restaurants(self):
-        return    
+        pass    
+
+
+#sfdsfsd
+
+if __name__=='__main__':
+        engine = create_engine('sqlite:///restaurants.db', echo=True)
+        Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
