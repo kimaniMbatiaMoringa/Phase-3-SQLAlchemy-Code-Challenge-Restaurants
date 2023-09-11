25546b66b11d73bb91c1ff170824a77b8be85e19
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-09-11 03:26:07 +0300

    semi functional restaurant table

--- lib/classes/Restaurant.py
+++ lib/classes/Restaurant.py
@@ -1,20 +1,57 @@
 from functools import reduce
 
-class Restaurant:   
-    def __init__(self,name):
+from sqlalchemy import (create_engine, desc, func,
+    CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
+    Index, Column, DateTime, Integer, String)
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Restaurant(Base):
+
+    __table_name__= 'restaurants'
+
+    __table_args__=(
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'
+        ),
+        UniqueConstraint(
+            'name',
+            name='unique_name'
+        )
+    )
+
+    id = Column(Integer(),primary_key=True)
+    name = Column(String())
+    price = Column(Integer())
+
+
+    def __init__(self,name,price):
         self.name = name
-        self.ratings = []
-        self.agg = 0
-        self.customer_list = []
+        #self.ratings = []
+        #self.agg = 0
+        #self.customer_list = []
+        self.price = price
+        engine = create_engine('sqlite:///restaurants.db', echo=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.add(self)
+        session.commit()
 
     def name(self):
         return self.name
     
     def average_star_rating(self):
+        pass
         self.agg =sum(self.ratings) / len(self.ratings)
         print(self.agg)
     
     def set_rating(self,value):
+        pass
         input = value
         self.ratings.append(input)
 
--- lib/main.py
+++ lib/main.py
@@ -1,9 +1,14 @@
 #from classes.Review import Review
 from classes.Customer import Customer
-#from classes.Restaurant import Restaurant
+from classes.Restaurant import Restaurant
 
 
 kimani_mbatia = Customer(
     name="Kimani",
     family="Mbatia",
+)
+
+mong_kok = Restaurant(
+    name="Mong Kok",
+    price=100
 )
